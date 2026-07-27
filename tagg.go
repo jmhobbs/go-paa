6458dca2f855a680b483e3efe892e296d91dfc22
John Hobbs <john@velvetcache.org>
2026-07-26 22:03:48 -0500

    Tagg encoding

--- tagg.go
+++ tagg.go
@@ -3,7 +3,10 @@ package paa
 import (
 	"encoding/binary"
 	"fmt"
+	"image"
 	"io"
+
+	"github.com/jmhobbs/go-paa/quantize"
 )
 
 var TaggSignature uint32 = 0x54414747
@@ -13,6 +16,8 @@ const (
 	Tagg_MAXC uint32 = 0x4d415843
 	Tagg_OFFS uint32 = 0x4f464653
 )
+
+/////////////////// AVGC
 
 type TaggAVGC struct {
 	Red   uint8
@@ -25,12 +30,16 @@ func (t TaggAVGC) String() string {
 	return fmt.Sprintf("R=%d, G=%d, B=%d, A=%d <#%2X%2X%2X%2X>", t.Red, t.Green, t.Blue, t.Alpha, t.Red, t.Green, t.Blue, t.Alpha)
 }
 
-type TaggMAXC struct {
-	Data [4]uint8
-}
-
-type TaggOFFS struct {
-	Offsets [16]uint32
+func (t TaggAVGC) Write(out io.Writer) error {
+	err := binary.Write(out, binary.LittleEndian, Tagg_AVGC)
+	if err != nil {
+		return err
+	}
+	err = binary.Write(out, binary.LittleEndian, uint32(4))
+	if err != nil {
+		return err
+	}
+	return binary.Write(out, binary.LittleEndian, t)
 }
 
 func DecodeTaggAVGC(in io.Reader) (*TaggAVGC, error) {
@@ -48,6 +57,65 @@ func DecodeTaggAVGC(in io.Reader) (*TaggAVGC, error) {
 	return &avgc, err
 }
 
+func CalculateAVGC(img image.Image) TaggAVGC {
+	var avgc [3]byte
+	switch img := img.(type) {
+	case *image.NRGBA:
+		avgc = quantize.MedianCut(fourChannelTothreeChannel(img.Pix))
+	case *image.RGBA:
+		avgc = quantize.MedianCut(fourChannelTothreeChannel(img.Pix))
+	default:
+		avgc = medianCutSlow(img)
+	}
+
+	return TaggAVGC{
+		Red:   avgc[0],
+		Green: avgc[1],
+		Blue:  avgc[2],
+		Alpha: 0xff,
+	}
+}
+
+func medianCutSlow(img image.Image) [3]byte {
+	buf := make([][3]byte, img.Bounds().Dx()*img.Bounds().Dy())
+
+	for y := range img.Bounds().Dy() {
+		rowoffset := y * img.Bounds().Dx()
+		for x := range img.Bounds().Dx() {
+			r, g, b, _ := img.At(x, y).RGBA()
+			buf[rowoffset+x] = [3]byte{uint8(r), uint8(g), uint8(b)}
+		}
+	}
+
+	return quantize.MedianCut(buf)
+}
+
+func fourChannelTothreeChannel(buf []byte) [][3]byte {
+	out := make([][3]byte, len(buf)/4)
+	for i := 0; i < len(buf); i += 4 {
+		out[i/4] = [3]byte{buf[i], buf[i+1], buf[i+2]}
+	}
+	return out
+}
+
+/////////////////// MAXC
+
+type TaggMAXC struct {
+	Data [4]uint8
+}
+
+func (t TaggMAXC) Write(out io.Writer) error {
+	err := binary.Write(out, binary.LittleEndian, Tagg_MAXC)
+	if err != nil {
+		return err
+	}
+	err = binary.Write(out, binary.LittleEndian, uint32(4))
+	if err != nil {
+		return err
+	}
+	return binary.Write(out, binary.LittleEndian, t)
+}
+
 func DecodeTaggMAXC(in io.Reader) (*TaggMAXC, error) {
 	var length uint32
 	err := binary.Read(in, binary.LittleEndian, &length)
@@ -61,6 +129,24 @@ func DecodeTaggMAXC(in io.Reader) (*TaggMAXC, error) {
 	var maxc TaggMAXC
 	err = binary.Read(in, binary.LittleEndian, &maxc)
 	return &maxc, err
+}
+
+/////////////////// OFFS
+
+type TaggOFFS struct {
+	Offsets [16]uint32
+}
+
+func (t TaggOFFS) Write(out io.Writer) error {
+	err := binary.Write(out, binary.LittleEndian, Tagg_OFFS)
+	if err != nil {
+		return err
+	}
+	err = binary.Write(out, binary.LittleEndian, uint32(16*4))
+	if err != nil {
+		return err
+	}
+	return binary.Write(out, binary.LittleEndian, t)
 }
 
 func DecodeTaggOFFS(in io.Reader) (*TaggOFFS, error) {
