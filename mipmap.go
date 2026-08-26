0dae2eaa68e5d21868678f5e1cc594e94c41b5a9
John Hobbs <john@velvetcache.org>
2026-08-26 11:17:49 -0500

    Work around lzo reader buffer limits

--- mipmap.go
+++ mipmap.go
@@ -1,6 +1,7 @@
 package paa
 
 import (
+	"bytes"
 	"fmt"
 	"image"
 	"io"
@@ -38,7 +39,50 @@ func (m *Mipmap) Reader(src io.ReadSeeker) (io.Reader, error) {
 	if !m.Compressed {
 		return &reader, nil
 	}
-	return lzo.NewReader(&reader), nil
+
+	compressed, err := io.ReadAll(&reader)
+	if err != nil {
+		return nil, err
+	}
+
+	size, err := m.decompressedSize()
+	if err != nil {
+		return nil, err
+	}
+
+	// lzo.NewReader as a bound buffer size of 64kb, which
+	// PAA images can easily overrun, so we have to eat it all
+	// in one go instead.
+	decompressed := make([]byte, size)
+	n, err := lzo.Decompress(compressed, decompressed)
+	if err != nil {
+		return nil, fmt.Errorf("error: failed to decompress lzo mipmap data: %w", err)
+	}
+
+	return bytes.NewReader(decompressed[:n]), nil
+}
+
+func (m *Mipmap) decompressedSize() (int, error) {
+	switch m.Type {
+	case Type_DXT1:
+		return blockCount(m.Width, m.Height) * 8, nil
+	case Type_DXT2, Type_DXT3, Type_DXT4, Type_DXT5:
+		return blockCount(m.Width, m.Height) * 16, nil
+	case Type_RGBA4, Type_RGBA5:
+		return int(m.Width) * int(m.Height) * 2, nil
+	case Type_RGBA8:
+		return int(m.Width) * int(m.Height) * 4, nil
+	case Type_Gray:
+		return int(m.Width) * int(m.Height), nil
+	default:
+		return 0, fmt.Errorf("error: unsupported mipmap type: %s", m.Type)
+	}
+}
+
+func blockCount(width, height uint16) int {
+	blockCountX := (int(width) + 3) / 4
+	blockCountY := (int(height) + 3) / 4
+	return blockCountX * blockCountY
 }
 
 func (m *Mipmap) Image(src io.ReadSeeker) (*image.NRGBA, error) {
